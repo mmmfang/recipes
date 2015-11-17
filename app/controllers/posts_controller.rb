be6fc2bebcbe215a5068e9d8fc3e199b00c9f311
mmmfang <mae.fang@gmail.com>
2015-11-17 00:51:45 -0500

    updating controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,24 @@
 class PostsController < ApplicationController
+
 	def index
-		@posts = Post.all
+		@posts = current_user.posts
 	end
 
 	def create
-		if session[:current_user_id]
-			current_user=User.find(session[:current_user_id])
-			post = current_user.posts.new(post_params)	
+		# if session[:current_user_id]
+		# 	current_user=User.find(session[:current_user_id])
+			@post = current_user.posts.new(post_params)	
 
-			if post.save
-				render json: post
+			if @post.save
+				# render json: post
 			else
 				render json {
 					error:true,
 					message: post.errors.full_messages.to_sentence
 				}
 			end
-		else
-			redirect_to root_path
-		end
-
+		# else
+		#end
 
 	end
 
